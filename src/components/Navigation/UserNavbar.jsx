7a3f15982cc00af1099ed93ccf00e240c618bf6a
Hardik Sorthiya <sorthiyahardik6869@gmail.com>
2022-03-13 17:40:18 +0530

    Ghanged Navabar Style

--- src/components/Navigation/UserNavbar.jsx
+++ src/components/Navigation/UserNavbar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { AppBar, Toolbar, Container } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
 
 const UserNavbar = () => {
@@ -8,19 +7,55 @@ const UserNavbar = () => {
   };
   return (
     <>
-      <Container fixed>
-        <AppBar position="static">
-          <Toolbar>
-            <NavLink to="/home">Home</NavLink>
-            <NavLink to="/post">AddItems</NavLink>
-            <NavLink to="/addnews">AllItems</NavLink>
-            <NavLink to="/userprofile">All Users</NavLink>
-            <NavLink to="/" onClick={() => LogoutHandler()}>
-              Logout
-            </NavLink>
-          </Toolbar>
-        </AppBar>
-      </Container>
+      <nav className="navbar navbar-expand-lg navbar-light bg-secondary ">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <i class="fa-solid fa-house-chimney"></i>
+              <NavLink className="navbar-brand nav-link" to="/home">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <i class="fa fa-signs-post"></i>
+              <NavLink className="navbar-brand nav-link" to="/post">
+                Posted
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <i class="fa-regular fa-newspaper"></i>
+              <NavLink className="navbar-brand nav-link" to="/addnews">
+                AddNews
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <i class="fa-regular fa-address-card"></i>
+              <NavLink className="navbar-brand nav-link" to="/userprofile">
+                UserProfile
+              </NavLink>
+            </li>
+          </ul>
+          <i class="fa-solid fa-right-from-bracket"></i>
+          <NavLink
+            className="navbar-brand pl-1"
+            to="/"
+            onClick={() => LogoutHandler()}
+          >
+            Logout
+          </NavLink>
+        </div>
+      </nav>
     </>
   );
 };
--- src/components/News/Home.jsx
+++ src/components/News/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import UserLayout from "../Navigation/UserLayout";
-
 function Home() {
   return (
     <UserLayout>
